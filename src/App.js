edbaa740bdd5b6a2aa7d5e3378d3f4d3ccdff89e
Simone Capuano <scapuano19@gmail.com>
2021-10-21 13:41:28 +0200

    pet details added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './components/navigation/Navigation';
+import PetDetailsPage from './page/details/PetDetailsPage';
+import PetDetailsNotFound from './page/detailsNotFound/PetDetailsNotFound';
 import HomePage from './page/home/Home';
 import SearchPage from './page/search/SearchPage';
 
@@ -9,8 +11,14 @@ function App() {
     <Router>
       <Navigation />
       <Switch>
+      <Route path='/pet-details-not-found'>
+      <PetDetailsNotFound />
+      </Route>
       <Route path='/search'>
       <SearchPage />
+      </Route>
+      <Route path='/:type/:id'>
+      <PetDetailsPage />
       </Route>
       <Route path='/:type?'>
       <HomePage />
