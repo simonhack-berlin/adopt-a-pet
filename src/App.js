2f69f7958188a9523728a168763a714aab1f3df2
Simone Capuano <scapuano19@gmail.com>
2021-10-21 13:05:03 +0200

    home page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './components/navigation/Navigation';
+import HomePage from './page/home/Home';
 
 function App() {
   return (
     <Router>
       <Navigation />
+      <Switch>
+      <Route path='/:type?'>
+      <HomePage />
+      </Route>
+      </Switch>
     </Router>
   );
 }
